afc89ffe0aa4305ae8e985796571d76b8bda31ef
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2021-11-17 00:05:35 -0500

    added more routes

--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,23 @@ router.get('/new', (req, res) => {
   res.send('User New Form');
 });
 
+router.post('/', (req, res) => {
+  res.send('Create User');
+});
+
+router
+  .route('/:id')
+  .get((req, res) => {
+    const userId = req.params.id;
+    res.send(`User Get with ID: ${userId}`);
+  })
+  .put((req, res) => {
+    const userId = req.params.id;
+    res.send(`User put with ID: ${userId}`);
+  })
+  .delete((req, res) => {
+    const userId = req.params.id;
+    res.send(`User Delete with ID: ${userId}`);
+  });
+
 module.exports = router;
