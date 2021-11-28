a005677c23a9088702d53b6011d7adad12cc1c70
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2021-11-27 20:27:21 -0500

    added fake data to retrieve with the api calls

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
 
+const posts = [
+  {
+    username: 'Juan',
+    title: 'Post 1',
+  },
+  {
+    username: 'Joanne',
+    title: 'Post 2',
+  },
+];
+
 app.get('/', (req, res) => {
   console.log('Here');
   res.json({ message: 'hey' });
