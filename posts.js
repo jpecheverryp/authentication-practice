f29118c220c80ae5af68d6aeefc4381fb36c782c
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2021-11-27 20:35:16 -0500

    moved posts to different file

---
+++ posts.js
@@ -0,0 +1,10 @@
+exports.array = [
+  {
+    username: 'Juan',
+    title: 'Post 1',
+  },
+  {
+    username: 'Joanne',
+    title: 'Post 2',
+  },
+];
--- server.js
+++ server.js
@@ -1,23 +1,14 @@
 const express = require('express');
 const app = express();
 
-const posts = [
-  {
-    username: 'Juan',
-    title: 'Post 1',
-  },
-  {
-    username: 'Joanne',
-    title: 'Post 2',
-  },
-];
-
 app.get('/', (req, res) => {
   console.log('Here');
   res.json({ message: 'hey' });
 });
 
 const userRouter = require('./routes/users');
+const postRouter = require('./routes/posts');
+app.use('/posts', postRouter);
 app.use('/users', userRouter);
 
 app.listen(3000);
